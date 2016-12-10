79023f98bf782bb9d51af62a5153491f5bce692b
Dom DiPasquale <domdipasquale@hotmail.com>
2016-12-09 23:18:01 -0500

    Play/Pause Toggle

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
 console.log('This would be the main JS file.');
+document.getElementById('now-playing-pause-button').style.display = 'none';
+
+document.getElementById('now-playing-play-button').addEventListener('click', function() {
+	document.getElementById('now-playing-play-button').style.display = 'none';
+	document.getElementById('now-playing-pause-button').style.display = 'unset';
+	document.getElementById('now-playing-repeat-toggle').innerText = 'REPEAT'
+}, false);
+
+document.getElementById('now-playing-pause-button').addEventListener('click', function() {
+	document.getElementById('now-playing-play-button').style.display = 'unset';
+	document.getElementById('now-playing-pause-button').style.display = 'none';
+	document.getElementById('now-playing-repeat-toggle').innerText = 'REPEATS'
+}, false);
